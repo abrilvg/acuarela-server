d81919a81e72f11ae268a94e01101b746ff955af
avril <amira.abril@gmail.com>
2019-07-29 00:03:57 -0300

    fixing route bug

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-// app.js
+const path = require('path');
+const cors = require('cors');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const mongoose = require('mongoose');
 
-const aquarelle = require('./routes/aquarelle.route'); // Imports routes for the acuarelas
-const users = require('./routes/user.route');
+const aquarelleRoutes = require('./routes/aquarelle.route'); // Imports routes for the acuarelas
+const usersRoutes = require('./routes/user.route');
+
+const port = process.env.PORT || 8080;
 const app = express();
 
 // Set up mongoose connection
-const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://localhost:27017/aquarelles';
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/aquarelles';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
@@ -19,11 +20,19 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/aquarelles', aquarelle);
-app.use('/users', users);
+app.use('/aquarelles', aquarelleRoutes);
+app.use('/users', usersRoutes);
 
-let port = 1234;
+//if (process.env.NODE_ENV === 'production') {
+// Serve any static files
+//TODO for prod### app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Handle React routing, return all requests to React app
+/*TODO for prod#### app.get('*', function (req, res) {
+  ####  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+#### });*/
+//}
 
 app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
+  console.log('Server is up and running on port numner ' + port);
 });
